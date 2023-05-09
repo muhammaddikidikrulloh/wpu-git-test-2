d156a1f57bbcf2049642b6105db9656ad844ee6f
muhammaddikidikrulloh <diki85811@gmail.com>
2023-05-09 09:10:54 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
   <h1>Hello World!</h1>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, eveniet repellat voluptate aliquid placeat cumque provident sint earum officia unde nam quae quia tempora aspernatur esse. Placeat ducimus eius est!</p>
+  <ul>
+    <li>satu</li>
+    <li>dua</li>
+  </ul>
 </body>
 </html>
